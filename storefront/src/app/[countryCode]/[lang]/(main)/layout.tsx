946bfa714ae35351294a6a5ed40deb31f852d3db
carlos19979 <carloscode23@icloud.com>
2026-01-24 17:33:27 +0100

    fix(storefront): force dynamic rendering to handle cart

--- storefront/src/app/[countryCode]/[lang]/(main)/layout.tsx
+++ storefront/src/app/[countryCode]/[lang]/(main)/layout.tsx
@@ -13,6 +13,9 @@ export const metadata: Metadata = {
   metadataBase: new URL(getBaseURL()),
 }
 
+export const dynamic = "force-dynamic"
+
+
 export default async function PageLayout({
   children,
   params,
